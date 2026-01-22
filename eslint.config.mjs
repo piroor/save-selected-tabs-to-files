f037175ccd50123cbc62cf50c607ec5648185a1a
YUKI Hiroshi <yuki@clear-code.com>
2026-01-22 12:58:13 +0900

    Format aggressively

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,8 @@
-import _import from "eslint-plugin-import";
-import { fixupPluginRules } from "@eslint/compat";
-import babelParser from "@babel/eslint-parser";
-import globals from "globals";
-import indentInParens from "eslint-plugin-indent-in-parens";
+import _import from 'eslint-plugin-import';
+import { fixupPluginRules } from '@eslint/compat';
+import babelParser from '@babel/eslint-parser';
+import globals from 'globals';
+import indentInParens from 'eslint-plugin-indent-in-parens';
 
 const languageOptions = {
   globals: {
@@ -12,110 +12,110 @@ const languageOptions = {
 
   // This is for a workaround to avoid parsing error around static properties defined in class definitions.
   // See also: https://stackoverflow.com/questions/42701440/eslint-parsing-error-unexpected-token-error-for-assigned-fat-arrow-prop
-  parser: babelParser,
+  parser:        babelParser,
   parserOptions: {
     requireConfigFile: false,
   },
 
   ecmaVersion: 2022,
-  sourceType: "module",
+  sourceType:  'module',
 };
 
 const rules = {
-  "no-const-assign": "error",
+  'no-const-assign': 'error',
 
-  "prefer-const": ["warn", {
-    destructuring: "any",
+  'prefer-const': ['warn', {
+    destructuring:          'any',
     ignoreReadBeforeAssign: false,
   }],
 
-  "no-var": "error",
+  'no-var': 'error',
 
-  "no-unused-vars": ["warn", {
-    vars: "all",
-    args: "after-used",
-    argsIgnorePattern: "^_",
-    caughtErrors: "all",
-    caughtErrorsIgnorePattern: "^_",
+  'no-unused-vars': ['warn', {
+    vars:                      'all',
+    args:                      'after-used',
+    argsIgnorePattern:         '^_',
+    caughtErrors:              'all',
+    caughtErrorsIgnorePattern: '^_',
   }],
 
-  "no-use-before-define": ["error", {
+  'no-use-before-define': ['error', {
     functions: false,
-    classes: true,
+    classes:   true,
   }],
 
-  "no-unused-expressions": "error",
-  "no-unused-labels": "error",
+  'no-unused-expressions': 'error',
+  'no-unused-labels':      'error',
 
-  "no-undef": ["error", {
+  'no-undef': ['error', {
     typeof: true,
   }],
 
-  indent: ["warn", 2, {
-    SwitchCase: 1,
+  indent: ['warn', 2, {
+    SwitchCase:       1,
     MemberExpression: 1,
 
     CallExpression: {
-      arguments: "first",
+      arguments: 'first',
     },
 
     VariableDeclarator: {
-      var: 2,
-      let: 2,
+      var:   2,
+      let:   2,
       const: 3,
     },
   }],
 
-  "no-underscore-dangle": ["warn", {
+  'no-underscore-dangle': ['warn', {
     allowAfterThis: true,
   }],
 
-  quotes: ["warn", "single", {
-    avoidEscape: true,
+  quotes: ['warn', 'single', {
+    avoidEscape:           true,
     allowTemplateLiterals: true,
   }],
 
-  "brace-style": ["warn", "stroustrup", { allowSingleLine: true }],
+  'brace-style': ['warn', 'stroustrup', { allowSingleLine: true }],
 
-  "keyword-spacing": ["warn", {
+  'keyword-spacing': ['warn', {
     overrides: {
       catch: { after: false },
     },
   }],
 
-  "space-before-function-paren": ["warn", {
-    anonymous: "never",
-    named: "never",
-    asyncArrow: "always",
+  'space-before-function-paren': ['warn', {
+    anonymous:  'never',
+    named:      'never',
+    asyncArrow: 'always',
   }],
 
-  "space-before-blocks": ["warn", "always"],
-  "func-call-spacing": ["warn", "never"],
-  "space-in-parens": ["warn", "never"],
-  "comma-spacing": ["warn", { before: false, after: true }],
-  "key-spacing": ["warn", { beforeColon: false, afterColon: true, align: "value" }],
-  "space-infix-ops": "warn",
-  "indent-in-parens/indent-in-parens": "warn",
+  'space-before-blocks':               ['warn', 'always'],
+  'func-call-spacing':                 ['warn', 'never'],
+  'space-in-parens':                   ['warn', 'never'],
+  'comma-spacing':                     ['warn', { before: false, after: true }],
+  'key-spacing':                       ['warn', { beforeColon: false, afterColon: true, align: 'value' }],
+  'space-infix-ops':                   'warn',
+  'indent-in-parens/indent-in-parens': 'warn',
 };
 
 const ESModuleFiles = [
-  "background/*.js",
-  "common/*.js",
-  "options/*.js",
-  "resources/module/*.js",
-  "sidebar/*.js",
-  "sidebar/components/*.js",
-  "tests/*.js",
+  'background/*.js',
+  'common/*.js',
+  'options/*.js',
+  'resources/module/*.js',
+  'sidebar/*.js',
+  'sidebar/components/*.js',
+  'tests/*.js',
 ];
 
 export default [{ // global
-  ignores: ["!**/.eslintrc.js", "**/submodules/", "**/extlib/", "**/eslint.config.mjs", "**/for-module.mjs"],
+  ignores: ['!**/.eslintrc.js', '**/submodules/', '**/extlib/', '**/for-module.mjs'],
 }, { // regular JS files
-  files: ["**/*.js", "**/*.mjs"],
+  files:   ['**/*.js', '**/*.mjs'],
   ignores: [...ESModuleFiles],
   languageOptions,
   plugins: {
-    "indent-in-parens": indentInParens,
+    'indent-in-parens': indentInParens,
   },
   rules,
 }, { // ES modules
@@ -124,14 +124,14 @@ export default [{ // global
   languageOptions,
 
   plugins: {
-    import: fixupPluginRules(_import),
-    "indent-in-parens": indentInParens,
+    import:             fixupPluginRules(_import),
+    'indent-in-parens': indentInParens,
   },
 
   settings: {
-    "import/resolver": {
-      "babel-module": {
-        root: ["./"],
+    'import/resolver': {
+      'babel-module': {
+        root: ['./'],
       },
     },
   },
@@ -140,35 +140,35 @@ export default [{ // global
     ...rules,
 
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/default.md
-    "import/default": "error",
+    'import/default':                    'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/namespace.md
-    "import/namespace": "error",
+    'import/namespace':                  'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
-    "import/no-duplicates": "error",
+    'import/no-duplicates':              'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/export.md
-    "import/export": "error",
+    'import/export':                     'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
-    "import/extensions": ["error", "always"],
+    'import/extensions':                 ['error', 'always'],
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
-    "import/first": "error",
+    'import/first':                      'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/named.md
-    "import/named": "error",
+    'import/named':                      'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
-    "import/no-named-as-default": "error",
+    'import/no-named-as-default':        'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default-member.md
-    "import/no-named-as-default-member": "error",
+    'import/no-named-as-default-member': 'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-cycle.md
-    "import/no-cycle": ["warn", {
+    'import/no-cycle':                   ['warn', {
       // If we comment out this, `maxDepth` is `Infinity`.
       //'maxDepth': 1,
     }],
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-webpack-loader-syntax.md
-    "import/no-self-import": "error",
+    'import/no-self-import': 'error',
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unresolved.md
-    "import/no-unresolved": ["error", {
+    'import/no-unresolved':  ['error', {
       caseSensitive: true,
     }],
     // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-useless-path-segments.md
-    "import/no-useless-path-segments": "error",
+    'import/no-useless-path-segments': 'error',
   },
 }];
